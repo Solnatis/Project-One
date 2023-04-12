d21df1153169b76576bc607ea21b41c360f83d38
bear-muna <bearmuna@gmail.com>
2023-04-12 14:06:46 -0700

    logic to enter a trap room

--- Assets/JS/gameplay.js
+++ Assets/JS/gameplay.js
@@ -2,6 +2,7 @@ var backgroundPicEL = document.querySelector('#background-pic');
 
 var riddleURL = "https://riddles-api.vercel.app/random";
 
+var roomNameEl = document.querySelector('#room-name');
 var promptEl = document.querySelector('#prompt');
 var optionA = document.querySelector('#option-a');
 var optionB = document.querySelector('#option-b');
@@ -13,22 +14,23 @@ var optionContainer = document.querySelector('#option-container');
 var choicesArray = [optionA, optionB, optionC, optionD];
 
 var room;
+var count = 0;
 
-// Trap room array will have riddles
-var trapRoom = {
-  // Entry into the room
-  entry: ["Pick the blue key", "Pick the red key", "Duck into a room"],
+// // Trap room array will have riddles
+// var trapRoom = {
+//   // Entry into the room
+//   entry: ["Pick the blue key", "Pick the red key", "Duck into a room"],
 
-  // Prompt / Riddle
-  riddle: undefined,
+//   // Prompt / Riddle
+//   riddle: undefined,
 
-  // Opt
-  opt: [optionA, optionB, optionC, optionD],
+//   // Opt
+//   opt: [optionA, optionB, optionC, optionD],
 
-  // Pic
-  pic: undefined,
+//   // Pic
+//   pic: undefined,
 
-}
+// }
 
 // Need to create objects and array of objects
 
@@ -38,7 +40,7 @@ var trapRoom = {
 var startRoom = {
 
 
-  roomname: "startRoom",
+  roomname: "Starter Room",
 
   // Entry property is how the user picks the room
   entry: ["X", "Y", "Z"],
@@ -56,7 +58,7 @@ var startRoom = {
 
 
 var roomEntry1 = {
-  roomname: "roomEntry1",
+  roomname: "Choose Your Room",
   prompt: ["Pick a door"],
   opt: ["Snake room", "Eagle room", "Hint room"],
   entry: ["Go into the depth"],
@@ -64,6 +66,7 @@ var roomEntry1 = {
 
 
 var snake = {
+  roomname: "Snake Room",
   entry: ["Snake room"],
   prompt: ["A pit of snakes!!"],
   opt: ["Swing on vine", "Try to jump!"],
@@ -72,6 +75,7 @@ var snake = {
 
 
 var eagle = {
+  roomname: "Eagle Room",
   entry: ["Eagle room"],
   prompt: ["Door with key slot"],
   opt: ["Pick the green key", "Pick the blue key", "Pick the red key"],
@@ -80,6 +84,7 @@ var eagle = {
 
 
 var hint = {
+  roomname: "Hint Room",
   entry: ["Hint room"],
   prompt: [""],
   opt: ["Receive hint"],
@@ -88,22 +93,24 @@ var hint = {
 
 
 var safe1 = {
-entry: ["Swing on vine", "Pick the green key"],
-prompt: ["You made it to your first Safe Room!"],
-opt: ["Proceed to the next room"],
-pic: undefined,
+  roomname: "Safe Room",
+  entry: ["Swing on vine", "Pick the green key"],
+  prompt: ["You made it to your first Safe Room!"],
+  opt: ["Proceed to the next room"],
+  pic: undefined,
 };
 
 var roomEntry2 = {
-  roomname: "roomEntry2",
+  roomname: "Choose Your Room",
   prompt: ["Pick a room"],
   opt: ["Boar room", "Dragon room", "Bear room"],
-  entry: ["Proceed to the next room", "Receive hint"],
+  entry: ["Proceed to the next room", "Receive hint", "Riddle Answer 1"],
   pic: undefined,
 };
 
 
 var boar = {
+  roomname: "Boar Room",
   entry: ["Boar room"],
   prompt: ["An angry minotaur!"],
   opt: ["Roll out of the way", "Try to stop him", "Side step"],
@@ -112,6 +119,7 @@ var boar = {
 
 
 var dragon = {
+  roomname: "Dragon Room",
   entry: ["Dragon room"],
   prompt: ["There's a sleeping dragon in this room! What to do?"],
   opt: ["Sneak past the dragon", "Take some treasure then leave!", "Wake him up because why not"],
@@ -120,6 +128,7 @@ var dragon = {
 
 
 var bear = {
+  roomname: "Bear Room",
   entry: ["Bear room"],
   prompt: ["Bear chase! Run!"],
   opt: ["Run to the end of the hall", "Duck into a room", "Play dead"],
@@ -127,6 +136,7 @@ var bear = {
 };
 
 var dragonTrap = {
+  roomname: "Dragon's Riddle",
   entry: ["Sneak past the dragon"],
   prompt: ["You woke the dragon! Solve this riddle to escape: Which animal can you make if you take; the head of a lamb, the middle of a pig, and the hind and tail of a dragon?"],
   opt: ["Lion", "Tiger", "Seahorse", "Hippo"],
@@ -134,6 +144,7 @@ var dragonTrap = {
 };
 
 var safe2 = {
+  roomname: "Safe Room",
   entry: ["Lion", "Side step", "Wake him up because why not", "Play dead"],
   prompt: ["Another safe room! Well done!"],
   opt: ["Pick another room"],
@@ -141,6 +152,7 @@ var safe2 = {
 };
 
 var roomEntry3 = {
+  roomname: "Choose Your Room",
   entry: ["Pick another room"],
   prompt: ["Choose your next room!"],
   opt: ["Riddle Room", "Hydra Room", "Angel Room"],
@@ -148,6 +160,7 @@ var roomEntry3 = {
 };
 
 var riddleRoom = {
+  roomname: "Riddle Room",
   entry: ["Riddle Room"],
   prompt: ["My challenge has made men throughout time stumble, I have defeated kings and left wise men humble, You see me now, but I am most often heard, and have killed men when with bullets paired, You might break a sweat when fighting with me, but I will exert no pressure on your body, and with a direct approach you can never find victory, work laterally rather than literally. What am I?"],
   opt: ["A riddle", "Time", "Age", "Happiness"],
@@ -155,6 +168,7 @@ var riddleRoom = {
 };
 
 var hydra = {
+  roomname: "Hydra Room",
   entry: ["Hydra Room"],
   prompt: ["There's a room filled with water and a hydra stands before you! What do you do?"],
   opt: ["Pick up a rusty spear and throw it", "Try to swim across", "Offer your treasure", "Say you know a dragon"],
@@ -162,6 +176,7 @@ var hydra = {
 };
 
 var angel = {
+  roomname: "Angel Room",
   entry: ["Angel Room"],
   prompt: ["The safest of all rooms and the richest! You found the temple's treaure!"],
   opt: ["Claim your prize", "Claim your prize", "Claim your prize", "Claim your prize"],
@@ -169,6 +184,7 @@ var angel = {
 };
 
 var victoryScreen = {
+  roomname: "VICTORY!",
   entry: ["A riddle", "Say you know a dragon", "Claim your prize"],
   prompt: ["YOU ESCAPED!"],
   explanation: ["You beat the temple's periless riddle!", "You managed to trick the hydra and got away safely!", "You walked out the temple a little bit richer"],
@@ -177,6 +193,7 @@ var victoryScreen = {
 };
 
 var gameoverScreen = {
+  roomname: "GAME OVER!",
   entry: ["Try to jump!", "Roll out of the way", "Try to stop him", "Take some treasure then leave!", "Run to the end of the hall", "Tiger", "Seahorse", "Hippo", "Time", "Age", "Happiness", "Pick up a rusty spear and throw it", "Try to swim across", "Offer your treasure"],
   prompt: ["GAME OVER"],
   explanation: ["Jumped a lil too short and the snakes got you", "You didn't roll far enough and got stomped", "What were you thinking?", "The treasure was death", "You can't outrun death!", "The temple claims another with its riddles", "The temple claims another with its riddles", "The temple claims another with its riddles", "The temple claims another with its riddles", "The temple claims another with its riddles", "The temple claims another with its riddles", "The spear bounces off the hydra's scales and now it's more mad", "What're you doing? You can't swim", "You have no tresure and the hydra is made"],
@@ -210,17 +227,21 @@ function trapRoomStorage() {
 }
 
 // Everytime renderTrap() is called, the trapRoomArray must get smaller by 1
-function renderTrap() {
+function renderTrap(obj) {
   console.log("This is your array: " + trapRoomArray);
 
-   promptEl.textContent = "";
-   promptEl.textContent = trapRoomArray[0].riddle;
+  roomNameEl.textContent = "";
+  roomNameEl.textContent = "Temple Riddle";
+
+  promptEl.textContent = "";
+  promptEl.textContent = obj.riddle;
 
   for (let i = 0; i < trapRoomArray.length; i++) {
-    trapRoom.opt[i].textContent = "";
-    trapRoom.opt[i].textContent = trapRoomArray[i].answer;
+    choicesArray[i].textContent = "";
+    choicesArray[i].textContent = trapRoomArray[i].answer;
   }
 
+  count++;
 }
 
 // API for pictures
@@ -254,6 +275,7 @@ function getRiddleAPI() {
     console.log(data);
     
     var randomRiddle = {
+      entry: ["Pick the blue key", "Pick the red key", "Duck into a room"],
       riddle: data.riddle,
       answer: data.answer,
     };
@@ -264,8 +286,8 @@ function getRiddleAPI() {
     // Why would we need to store the trap array in local storage?
     // Might be over complicating things
     if (trapRoomArray.length === 4) {
+      renderRoom(startRoom);
       localStorage.setItem('trapRoomArray', JSON.stringify(trapRoomArray));
-      renderTrap();
     }
   })
 };
@@ -275,12 +297,16 @@ function startGame() {
   var confirm = localStorage.getItem('start-game');
   
   if (confirm) {
-    renderRoom(startRoom);
+    trapRoomGenerator();
+    // renderRoom(startRoom);
   };
 
 }
 
 function renderRoom(obj) {
+  roomNameEl.textContent = "";
+  roomNameEl.textContent = obj.roomname;
+  
   promptEl.textContent = "";
   promptEl.textContent = obj.prompt[0];
 
@@ -296,15 +322,18 @@ function renderRoom(obj) {
 function roomSelection(e) {
   e.preventDefault();
 
-  var click = e.target;
+  var click = e.target; 
+  var go = true;
 
   if (click.matches(".option")) {
-
+    
+  
+    
     for (let i = 0; i < roomArray.length; i++) {
       if (roomArray[i].entry.includes(click.textContent)) {
         room = roomArray[i];
         console.log(room);
-
+        go = false;
         if (room === victoryScreen) {
           localStorage.setItem('victory', room);
           location.href = 'gameEndScreens.html';
@@ -314,16 +343,18 @@ function roomSelection(e) {
         break;
       } 
     }
-
-
+    
     // Since trap room array is not populated prior to click, then you need to make array full
-    // for (let i = 0; i < trapRoomArray.length; i++); {
-    //   if (trapRoomArray[i].entry.includes(click.value)) {
-    //     room = trapRoomArray[i];
-    //     trapRoomGenerator();
-    //   }
-    // }
+    for (let i = count; i < trapRoomArray.length; i++) {
+      if (trapRoomArray[i].entry.includes(click.textContent) && go) {
+        room = trapRoomArray[i];
 
+        console.log(room);
+        renderTrap(room);
+        break;       
+      }
+    }
+    
 
         // Need to incorporate endgame objects for local storage
 
